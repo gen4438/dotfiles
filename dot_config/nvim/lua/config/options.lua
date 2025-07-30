5c6a0d14cb7ddb2d2d2284b9c922f2e71c93e93f
gen <g1e2n04@gmail.com>
2025-07-30 17:05:17 +0900

    fix(windows): set SHELL env for :terminal fallback shells
    Ensure the SHELL environment variable is set for all fallback shells
    on Windows, including msys bash, pwsh, powershell, and cmd. This
    improves compatibility for :terminal and shell commands in Neovim.

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -119,23 +119,28 @@ if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
     vim.opt.shellcmdflag = '-c'
     vim.opt.shellquote = ''
     vim.opt.shellxquote = ''
+    -- Set SHELL environment variable for :terminal
+    vim.env.SHELL = msys_bash
   -- Fallback to PowerShell
   elseif vim.fn.executable('pwsh') == 1 then
     vim.opt.shell = 'pwsh'
     vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
     vim.opt.shellquote = ''
     vim.opt.shellxquote = ''
+    vim.env.SHELL = 'pwsh'
   elseif vim.fn.executable('powershell') == 1 then
     vim.opt.shell = 'powershell'
     vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
     vim.opt.shellquote = ''
     vim.opt.shellxquote = ''
+    vim.env.SHELL = 'powershell'
   -- Final fallback to cmd
   else
     vim.opt.shell = 'cmd'
     vim.opt.shellcmdflag = '/c'
     vim.opt.shellquote = ''
     vim.opt.shellxquote = '"'
+    vim.env.SHELL = 'cmd'
   end
 end
 
